77817fb74fa52f98aec9657be2400753b0cdb47c
Sampo Pelto <pelto.sampo@gmail.com>
2022-12-15 18:58:11 +0200

    ToInteger test implement

---
+++ test/toInteger.test.js
@@ -0,0 +1,14 @@
+import {expect} from 'chai';
+
+import toInteger from '../src/toInteger.js'
+
+describe('toInteger.js', function(){
+    it('toInteger 3.2, should return 3', function(){
+        expect(toInteger(3.2)).to.equal(3)
+    });
+
+    it('Test toInteger -2.3, should return -2', function(){
+        expect(toInteger(-2.3)).to.equal(-2)
+    });
+
+});
