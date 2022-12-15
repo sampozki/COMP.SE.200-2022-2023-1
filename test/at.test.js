c6792568ecf8d2661242b7e285fbf1e501b5b49a
Sampo Pelto <pelto.sampo@gmail.com>
2022-12-15 22:42:27 +0200

    at test implement

---
+++ test/at.test.js
@@ -0,0 +1,13 @@
+import {expect} from 'chai';
+
+import at from '../src/at.js'
+
+const object = { 'a': [{ 'b': { 'c': 3 } }, 4] }
+
+describe('at.js', function(){
+    it('test at() function using its test case', function(){
+        expect(at(object, ['a[0].b.c', 'a[1]'])).to.eql([3, 4])
+    });
+
+
+});
