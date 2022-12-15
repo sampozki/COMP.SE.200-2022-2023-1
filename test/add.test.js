24da2bcada322dcb9db00539834150e21222d73e
Sampo Pelto <pelto.sampo@gmail.com>
2022-12-15 18:58:18 +0200

    Refactor

--- test/add.test.js
+++ test/add.test.js
@@ -2,21 +2,16 @@ import {expect} from 'chai';
 
 import add from '../src/add.js'
 
-
-
 describe('Add.js', function(){
     it('Sum two positive integers', function(){
-
         expect(add(2, 1)).to.equal(3)
     });
 
     it('Sum two negative integers', function(){
-
         expect(add(-2, -1)).to.equal(-3)
     });
 
     it('2+3 should not be 4', function(){
-
         expect(add(2, 3)).to.not.equal(3)
     });
 
